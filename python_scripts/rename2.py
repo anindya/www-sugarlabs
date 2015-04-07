53501d81b280ce5ad515f530cc45938e9645f8a3
Anindya Chakravarti <anindyac.iitr@gmail.com>
2015-04-07 19:25:21 +0530

    Fixed some issues related to last commit

--- python_scripts/rename2.py
+++
@@ -1,7 +0,0 @@
-import os
-import re
-dir = "/home/techgeek/Projects/www-sugarlabs/python_scripts/sugarlabs_Activites/"
-files = os.listdir(dir)
-for file_l in files:
-	if file_l[-1]!="\W":
-		os.rename(file_l, file_l[:-1])
